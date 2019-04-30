1ac3b94016f4d9d22e17850378375458af7aa005
eralpkor <eralp.ny@gmail.com>
2019-04-29 20:13:15 -0400

    Updates

---
+++ precache-manifest.d0e3b57566b1295475490bbd00bf1799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "1a3e2a9a6b67fcbdc815",
+    "url": "/react-lights-out/static/js/main.1a3e2a9a.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/react-lights-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "1a3e2a9a6b67fcbdc815",
+    "url": "/react-lights-out/static/css/main.ca540222.chunk.css"
+  },
+  {
+    "revision": "952018bd04302d3b1f43ebd3ac730be7",
+    "url": "/react-lights-out/index.html"
+  }
+];
